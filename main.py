1c6243643d3443f211dc413d7f3269c3d8ac509f
Caio Vinícius Araújo Ramos <aramoscaio@gmail.com>
2023-08-03 01:14:24 -0300

    fix: present time set and autoclick start before clock timer starts

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import random
 import datetime
 from click_timer import click_timer
-from autoclick import autoclick_regfreq
+from autoclick import autoclick_reg
 
 password = input('Senha: ')
 
 while True:
-    t = random.randrange(10, 15)
+    now = datetime.datetime.now()
+    autoclick_reg(password)
+    t = random.randrange(1770, 1860)
     print(f'Próxima senha em {t//60:02d}:{t%60:02d}')
     click_timer(t)
-    autoclick_regfreq(password)
-    now = datetime.datetime.now()
     print(f'Ultima senha inserida às {now.strftime("%H:%M")}')
     
