30d4b584b3ddec4b2f19201fad57c7a0f2786f53
Caio Vinícius Araújo Ramos <105233742+ARamosCaio@users.noreply.github.com>
2023-08-17 22:24:18 -0300

    Update main.py
    Decreased time range

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ password = input('Senha: ')
 while True:
     now = datetime.datetime.now()
     autoclick_reg(password)
-    t = random.randrange(1770, 1830)
+    t = random.randrange(1800, 1830)
     print(f'Próxima senha em {t//60:02d}:{t%60:02d}')
     click_timer(t)
     print(f'Ultima senha inserida às {now.strftime("%H:%M")}')
